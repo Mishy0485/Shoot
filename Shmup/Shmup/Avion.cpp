6b91c24d87cd944b831f06c512c169155a8d4c45
Julia <ayite.julia06@gmail.com>
2024-12-03 15:31:40 +0100

    restart

---
+++ Shmup/Shmup/Avion.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class Plane
+{
+private:
+	int x, y;
+	int vie;
+	Texture texture;
+	int vitesse;
+
+public:
+
+	Plane(int posX, int posY, int v, Texture tex, int vit) : x(posX), y(posY), vie(v), texture(tex), vitesse(vit) {}
+
+};
