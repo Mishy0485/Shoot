12ed501f2fb85f41fff7ec99c9fd3fdfbc7cb78d
jayite <jayite@gaming.tech>
2024-12-10 17:57:19 +0100

    boss picolo

--- Shmup/Shmup/boss2.cpp
+++ Shmup/Shmup/boss2.cpp
@@ -17,6 +17,7 @@ void Boss2::capaciteSimple(vector<Projectile*>& bulleta, Projectile Bullet)
 {
 	capsmpl1.loadFromFile("laser_gen.png");
 	laser_gen.setTexture(capsmpl1);
+	bulleta.push_back(new Projectile(general.getPosition().x, general.getPosition().y, 20, 0, false));
 }
 
 void Boss2::capaciteSpe(Jeu jeu)
